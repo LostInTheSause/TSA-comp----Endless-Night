b593fe977240685ba62730f492bd01cab87c53a9
LostInTheSause <evancboles@gmail.com>
2025-11-27 12:16:17 -0500

    Added the mushroom obstacle
    still trying to get it to actually apply the upward velocity correctly

---
+++ TSA comp/dusk-to-dawn/bouncy_mushroom.gd
@@ -0,0 +1,38 @@
+extends Area2D
+
+@onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
+@onready var lil_guy: CharacterBody2D = $"../Lil guy"
+
+
+const bounce_velocity: int = 600
+const bounce_direction: Vector2 = Vector2.UP
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	animated_sprite_2d.frame = 0
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+	
+
+
+
+func _on_body_entered(body: Node2D) -> void:
+	if body.is_in_group("Player"):
+		animated_sprite_2d.play("bounce_animation")
+	
+
+
+
+
+
+
+func _on_body_exited(body: Node2D) -> void:
+	if body.is_in_group("Player"):
+		while !animated_sprite_2d.animation_finished:
+			pass
+		animated_sprite_2d.stop()
+			
+		
