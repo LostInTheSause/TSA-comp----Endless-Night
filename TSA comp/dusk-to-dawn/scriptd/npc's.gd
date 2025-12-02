61ee4454bc442a1c4668a32e34bfdc80213c73a0
LostInTheSause <evancboles@gmail.com>
2025-12-02 16:50:04 -0500

    added a plugin called dialogic to help with dialogue

--- TSA comp/dusk-to-dawn/scriptd/npc's.gd
+++ TSA comp/dusk-to-dawn/scriptd/npc's.gd
@@ -1,6 +1,6 @@
 extends Node2D
 
-@onready var speaking: Control = $speaking
+
 @onready var animated_sprite_2d: AnimatedSprite2D = $"Npc charecter Body/AnimatedSprite2D"
 @onready var prespeak_label: RichTextLabel = $"Npc charecter Body/AnimatedSprite2D/prespeak_Label"
 @onready var area_2d: Area2D = $"Npc charecter Body/Area2D"
@@ -14,7 +14,7 @@ extends Node2D
 
 @export var NPC_idle_animation: SpriteFrames 
 @export var NPC_talking_animation: bool = false
-	
+var can_talk = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -23,18 +23,25 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
+	if can_talk:
+		if Input.is_action_pressed("talk"):
+				lil_guy.is_talking = true
+				Dialogic.start("opening_imeline")
+				
+				
 
 
 func _on_area_2d_body_entered(body: Node2D) -> void:
-	if body == lil_guy:
+	if body.is_in_group("Player"):
+		can_talk = true
 		prespeak_label.show()
-		if Input.is_action_just_pressed("talk"):
-			speaking.z_index = 2
-			speaking.show()
+		body.is_talking = true
+			
+			
 			
 			
 
 func _on_area_2d_body_exited(body: Node2D) -> void:
 	if body == lil_guy:
 		prespeak_label.hide()
+		can_talk = false
