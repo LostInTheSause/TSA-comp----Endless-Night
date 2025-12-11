a60f25700581ae70a981cb43debb691f6cf90409
LostInTheSause <evancboles@gmail.com>
2025-12-11 17:31:42 -0500

    finally started building out the tutorial
    a lot of minor changes to make it more balanced specifically to the dashing, fixed coyote time and made some new scenes to make my life easier
    a lot of good stuff done today,e

---
+++ TSA comp/dusk-to-dawn/scriptd/spike_pit.gd
@@ -0,0 +1,17 @@
+extends Node2D
+
+@export var how_many_spikes_in_pit: int = 0
+
+const spikes = preload("res://scenes/spikes.tscn")
+var move_amt = 35
+var y_value= self.position.y
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	if how_many_spikes_in_pit > 1:
+		for i in range(how_many_spikes_in_pit):
+			var new_spikes = spikes.instantiate()
+			add_child(new_spikes)
+			new_spikes.position = Vector2(i * move_amt, y_value)
+
+
+# Called every frame. 'delta' is the elapsed time 
