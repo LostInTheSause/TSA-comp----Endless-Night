cf30b310c0e6469a42205980b5ff06713f5f4b33
LostInTheSause <evancboles@gmail.com>
2025-12-13 14:23:30 -0500

    still beginning cutscene
    trying to make it fit better with the music but starting to work on scene transitions and such

--- TSA comp/dusk-to-dawn/scenes/opening_scene.gd
+++ TSA comp/dusk-to-dawn/scenes/opening_scene.gd
@@ -1,23 +1,37 @@
 extends Node2D
 @onready var node_2d: Node2D = $Node2D/Node2D
 @onready var shooting_star: AnimatedSprite2D = $Node2D/Node2D/shooting_star
+@onready var audio_stream_player_2d: AudioStreamPlayer2D = $AudioStreamPlayer2D
 
+@onready var button: Button = $Button
 
 @onready var camera_2d: Camera2D = $Camera2D
 @onready var animation_player: AnimationPlayer = $Node2D/Node2D/shooting_star/AnimationPlayer
 
 var starting_pos = Vector2(0.0, -43.0)
+var pan_up = false
+var button_in = false
+var button_amt = 0.1
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	camera_2d.position = starting_pos
 	Dialogic.start("opening_imeline")
 	Dialogic.signal_event.connect(on_dialogic_signal)
-
+	audio_stream_player_2d.playing = true
+	button.modulate = Color(1,1,1,0)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
+	if pan_up:
+		camera_2d.position = starting_pos
+		starting_pos.y -= 10 * delta
+		if starting_pos.y <= -207.0:
+			pan_up = false
+			button_in = true
+	if button_in:
+		button.modulate = Color(1,1,1,int(button_amt*delta))
+		button_amt += 0.1
 
 
 func  on_dialogic_signal(argument: String) -> void:
@@ -25,9 +39,10 @@ func  on_dialogic_signal(argument: String) -> void:
 		node_2d.visible = true
 		shooting_star.play("shoot_star")
 		animation_player.play("flying_star")
-		animation_player.animation_finished.connect(star_shooting_done)
-	elif argument == "show_title":
-		pass
+		await animation_player.animation_finished
+		node_2d.visible = false
+	elif argument == "Show_title":
+		pan_up = true
 		
 func star_shooting_done() -> void:
 	node_2d.visible = false
